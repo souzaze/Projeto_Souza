bd96665b1aa69c953541329e560eb90a80eafd1e
souzaze <souzaphani@gmail.com>
2024-12-12 11:17:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 // Função para enviar formulário de resenha e exibir uma mensagem de sucesso
 const formResenha = document.querySelector("form");
 
-if (formResenha) {
-    formResenha.addEventListener("submit", function(event) {
-        event.preventDefault(); // Previne o envio real do formulário
-        alert("Sua resenha foi enviada com sucesso!");
-        formResenha.reset(); // Reseta os campos do formulário após o envio
-    });
-}
-
 // Função para adicionar interatividade nas ofertas (mudança de cor ao passar o mouse)
 const ofertasItens = document.querySelectorAll("#ofertas .book-item");
 
