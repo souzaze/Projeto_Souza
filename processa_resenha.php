8bfb81339f7140b89766e3683f1d7655d104264b
souzaze <souzaphani@gmail.com>
2024-12-12 11:16:50 -0300

    Update processa_resenha.php

--- processa_resenha.php
+++ processa_resenha.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "ifsp";
+$password = "";
 $dbname = "sousas_literary";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
